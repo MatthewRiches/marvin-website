825568ccce9180abcafbe2bce9296d459d956959
Matthew Riches <matthew@vocaliq.com>
2015-05-26 09:37:29 +0100

    Added comments to JS

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-/* global */
+/* global variables */
 var timeout, slideshow = null;
+/* when document is ready */
 $(document).ready(function(e) {
+	/* toggles the navigation and sets data that the user did this */
 	$('button').click(function(e) {
 		$(this).toggleClass('close');
 		$('#nav').toggleClass('active').data('user', '');
 	});
+	/* for all links that begin with a hash, animate to that hash */
 	$('a[href^="#"]:not(.page)').click(function(e) {
 			var hash = $(this).attr('href');
 			$('html, body').animate({
@@ -15,6 +18,7 @@ $(document).ready(function(e) {
 			e.preventDefault();
 			return false;
 	});
+	/* generic pager control for section that have multiple items, like testimonials */
 	$('.pager a').click(function(e){
 		var index = $(this).index();
 		$(this).parent().find('a').removeClass('active');
@@ -23,13 +27,17 @@ $(document).ready(function(e) {
 		e.preventDefault();
 		return false;
 	})
+	/* run the slideshow function every 5 secons (the phone shows different screenshots) */
 	slideshow = setInterval(function(){slide()}, 5000);
 });
+/* if the hash changes without a click animate to it */
 $(window).on('hashchange', function() {
 	$('html, body').animate({scrollTop: $(window.location.hash).offset().top-$('#nav').height()}, 500);
 	e.preventDefault();
 	return false;
 });
+/* when the windows is scrolled the navigation will show if the screen is above tablet size
+and the user hasn't already opened the menu. */
 $(window).scroll(function () { 
     if (!timeout && typeof $('#nav').data('user') == 'undefined' && $(window).width() > 1024) {
         timeout = setTimeout(function () {
@@ -45,6 +53,8 @@ $(window).scroll(function () {
         }, 1000);
     }
 });
+/* slideshow, fade out the last one and then insert is at the start. Items are positioned 
+absolutely so the last child will be on top going backwards. */
 var slide = function(){
 	$('.slideshow img:last').fadeOut(1000, function(){$(this).insertBefore($('.slideshow img:first').show())});
 }
